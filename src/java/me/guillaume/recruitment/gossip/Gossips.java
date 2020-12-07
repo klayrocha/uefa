fc2e56474cba0de9bbb7cdd848486a2504bb3320
mobilebaas2020 <mobilebaas2020@gmail.com>
2020-12-07 19:45:08 +0000

    first commit

---
+++ src/java/me/guillaume/recruitment/gossip/Gossips.java
@@ -0,0 +1,28 @@
+package me.guillaume.recruitment.gossip;
+
+public class Gossips {
+
+	public Gossips(String ...newPersons) {
+	}
+	
+	public Gossips from(String from) {
+		return this;
+	}
+	
+	public Gossips to(String to) {
+		return this;
+	}
+	
+
+	public Gossips say(String message) {
+		return this;
+	}
+
+	public String ask(String string) {
+		return null;
+	}
+
+	public void spread() {
+		
+	}
+}
